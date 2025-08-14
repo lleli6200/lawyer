040e49dd809fbfc7c1410da85002f20a3f0a6ca0
leoleli <leoleli3@outlook.com>
2025-08-14 12:04:56 -0300

    Aumentar tamanho da logo

--- src/react-app/components/Header.tsx
+++ src/react-app/components/Header.tsx
@@ -38,7 +38,7 @@ export default function Header() {
               <img 
                 src="/images/WhatsApp Image 2025-07-24 at 12.11.39-Photoroom.png" 
                 alt="CFL Logo" 
-                className="w-12 h-12 object-contain"
+                className="w-16 h-16 object-contain"
               />
             </div>
 
